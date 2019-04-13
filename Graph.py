535734db3a52ed546ef2fbcc2efed35f4930e3c3
Miłosz Chodkowski <adeeeq77@gmail.com>
2019-04-13 21:07:32 +0200

    Final

--- Graph.py
+++ Graph.py
@@ -4,60 +4,42 @@ import random
 
 
 class Graph:
-    def __init__(self, nvert=None):
+    def __init__(self, nvert = 3):
         self.nvert = nvert  # number of vertices
         self.matrix = None
         self.listOfEdges = []
         self.listOfAdj = [[] for i in range(self.nvert)]  # empty matrix
-        self.visitedVert = [[] for i in range(self.nvert)]
 
     def generateMatrix(self):
-        nvert = self.nvert
-        matrix = [[random.randint(0, 1) for i in range(nvert)] for i in range(nvert)]
-        for i in range(nvert):
-            matrix[i][i] = 0
-        self.matrix = matrix
+        self.matrix = [[random.randint(0, 1) for i in range(self.nvert)] for i in range(self.nvert)]
+        for i in range(self.nvert):
+            self.matrix[i][i] = 0
 
     def printMatrix(self):
-        matrix = self.matrix
-        count = 0
         print("\nMatrix:")
-        for cell in matrix:
-            print(count, cell)
-            count += 1
+        for c, cell in enumerate(self.matrix, 1):
+            print(c, cell)
 
     def generateListOfEdges(self):
-        nvert = self.nvert
-        matrix = self.matrix
-        listofedges = self.listOfEdges
-        for i in range(nvert):
-            for j in range(nvert):
-                if matrix[i][j] == 1:
-                    listofedges.append((i, j))
-        self.listOfEdges = listofedges
+        for i in range(self.nvert):
+            for j in range(self.nvert):
+                if self.matrix[i][j] == 1:
+                    self.listOfEdges.append((i, j))
 
     def printListOfEdges(self):
-        listofedges = self.listOfEdges
         print("\nList of edges:")
-        for edge in listofedges:
+        for edge in self.listOfEdges:
             print(edge, end=" ")
 
     def generateListOfAdj(self):
-        nvert = self.nvert
-        matrix = self.matrix
-        listofadj = self.listOfAdj
-        for i in range(nvert):
-            for j in range(nvert):
-                if matrix[i] == 0:
-                    listofadj[i].append(None)
-                if matrix[i][j] == 1:
-                    listofadj[i].append(j)
-        self.listOfAdj = listofadj
+        for i in range(self.nvert):
+            for j in range(self.nvert):
+                if self.matrix[i] == 0:
+                    self.listOfAdj[i].append(None)
+                if self.matrix[i][j] == 1:
+                    self.listOfAdj[i].append(j)
 
     def printListOfAdj(self):
-        listofadj = self.listOfAdj
         print("\n\nList of Adj:")
-        count = 0
-        for adj in listofadj:
-            print(count, adj)
-            count += 1
+        for c, adj in enumerate(self.listOfAdj, 1):
+            print(c, adj)
